ef43b44a471d7f09e3ddf03be4463a473b707e2f
deepti-webonise <deepti@weboniselab.com>
2013-01-23 12:39:00 +0530

    devise added to person model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventManagement::Application.config.secret_token = 'fb67d381ca0b5131334d05453189e4cb9f3e106023b2b666ae4abedecda8277f9a5daf03128656148064473fb5fbdf058b1c176f7f9b93dddc49ed50ed176d4d'
